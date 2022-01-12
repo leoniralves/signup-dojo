f7ebeff1fbbfbc701292466cd66d29293179ce5b
Rafael Felipe <rafael.felipe1989@gmail.com>
2022-01-12 09:47:31 -0300

    organize strings

---
+++ FormsAndNavigation-Dojo/Scenes/SignUp/Commons/Constants+Strings.swift
@@ -0,0 +1,25 @@
+//
+//  Constants+Strings.swift
+//  FormsAndNavigation-Dojo
+//
+//  Created by Kauê Sales on 12/01/22.
+//
+
+import Foundation
+
+enum Constants {
+    struct SignUpStrings {
+        static let title: String = "Teste"
+        static let placeholderFirstName: String = "Nome"
+        static let placeholderLastName: String = "Sobrenome"
+        static let placeholderAge: String = "Idade"
+        static let placeholderEmail: String = "Email"
+        static let placeholderPassword: String = "Senha"
+        
+        static let buttonSignUpTitle: String = "Cadastrar"
+        
+        static let analyticsSignUpSuccess: String = "SignUp-Success"
+        static let analyticsSignUpFailed: String = "SignUp-Failed"
+        static let analyticsSignUpError: String = "SignUp-Error"
+    }
+}
