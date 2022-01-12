64da38184a4fbb4d8ca16e33fe36f15f6cf4033b
Kaue Sales <kaue.sbsousa@gmail.com>
2022-01-12 09:33:20 -0300

    :art: adding Constants enumeration

---
+++ FormsAndNavigation-Dojo/Commons/Constants+Strings.swift
@@ -0,0 +1,14 @@
+//
+//  Constants+Strings.swift
+//  FormsAndNavigation-Dojo
+//
+//  Created by Kauê Sales on 12/01/22.
+//
+
+import Foundation
+
+enum Constants {
+    struct SignUpStrings {
+        
+    }
+}
