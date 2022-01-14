059637234d10cbe643a9517c2a61c7bdb7227578
Gabriel Henrique <gabhenrique0@gmail.com>
2022-01-14 09:21:46 -0300

    :sparkles: Feat: create a view to abstract view from sign up controller

---
+++ FormsAndNavigation-Dojo/Scenes/SignUp/Components/SignUpView.swift
@@ -0,0 +1,19 @@
+//
+//  SignUpView.swift
+//  FormsAndNavigation-Dojo
+//
+//  Created by Gabriel Pereira on 14/01/22.
+//
+
+import UIKit
+
+final class SignUpView: UIView {
+    // MARK: - Override Methods
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
