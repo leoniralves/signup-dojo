1de8f25a088d210549b4035d24bb82f405f2e473
Gabriel Henrique <gabhenrique0@gmail.com>
2022-01-19 09:51:40 -0300

    :white_check_mark: Test: add all test cases to analytics send method

--- FormsAndNavigation-DojoTests/Scenes/SignUp/SignUpPresenterTests.swift
+++ FormsAndNavigation-DojoTests/Scenes/SignUp/SignUpPresenterTests.swift
@@ -9,30 +9,59 @@ import XCTest
 @testable import FormsAndNavigation_Dojo
 
 final class SignUpPresenterTests: XCTestCase {
-    
+    // MARK: - Properties
     private var analyticsSpy: AnalyticsSpy = .init()
 
     private lazy var sut: SignUpPresenter = {
         return .init(analytics: analyticsSpy)
     }()
     
+    // MARK: - Test Methods
     func test_trackNetworkRequest_whenResultSuccess_andDataTrue_shouldSendSignUpSuccess() {
         sut.trackNetworkRequest(result: .success(true))
         
-
-        XCTAssertEqual(analyticsSpy.sendArgs.count, 1)
-        XCTAssertEqual(analyticsSpy.sendArgs.first, "SignUp-Success")
+        thenAssertAnalyticsWasCalledOnce(event: "Success")
     }
     
     func test_trackNetworkRequest_whenResultSuccess_andDataFalse_shouldSendSignUpFailed() {
         sut.trackNetworkRequest(result: .success(false))
         
-
-        XCTAssertEqual(analyticsSpy.sendArgs.count, 1)
-        XCTAssertEqual(analyticsSpy.sendArgs.first, "SignUp-Failed")
+        thenAssertAnalyticsWasCalledOnce(event: "Failed")
     }
     
     func test_trackNetworkRequest_whenResultFailure_andHasError_shouldSendSignUpFailed() {
+        sut.trackNetworkRequest(result: .failure(DummyError.dummy))
         
+        thenAssertAnalyticsWasCalledOnce(event: "Error")
+    }
+}
+
+// MARK: - Mock
+extension SignUpPresenterTests {
+    enum DummyError: Error {
+        case dummy
+    }
+}
+
+// MARK: - Assertion Methods
+extension SignUpPresenterTests {
+    func thenAssertAnalyticsWasCalledOnce(
+        event: String,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        XCTAssertEqual(
+            analyticsSpy.sendArgs.count,
+            1,
+            file: file,
+            line: line
+        )
+        
+        XCTAssertEqual(
+            analyticsSpy.sendArgs.first,
+            "SignUp-\(event)",
+            file: file,
+            line: line
+        )
     }
 }
