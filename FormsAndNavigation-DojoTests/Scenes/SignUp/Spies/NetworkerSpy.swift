f775d66c401378bdd28c49858eef7d345d0f0b44
Gabriel Henrique <gabhenrique0@gmail.com>
2022-03-09 09:28:44 -0300

    :white_check_mark: Test: add verify method once on networker spy

--- FormsAndNavigation-DojoTests/Scenes/SignUp/Spies/NetworkerSpy.swift
+++ FormsAndNavigation-DojoTests/Scenes/SignUp/Spies/NetworkerSpy.swift
@@ -7,10 +7,47 @@
 
 import Foundation
 @testable import FormsAndNavigation_Dojo
+import XCTest
 
 final class NetworkerSpy: NetworkerProtocol {
-//    private(set) var
-    func request(target: Networker.Target, completion: (Result<Bool, Error>) -> Void) {
-        completion(.success(true))
+    // MARK: - Properties
+    private(set) var verifyRequestArgs: [Networker.Target] = []
+    var completionToBeReturned: Result<Bool, Error> = .success(true)
+    
+    // MARK: - NetworkerProtocol Methods
+    func request(
+        target: Networker.Target,
+        completion: (Result<Bool, Error>) -> Void
+    ) {
+        verifyRequestArgs.append(target)
+        
+        completion(completionToBeReturned)
+    }
+}
+
+// MARK: - Verifier Methods
+extension NetworkerSpy {
+    func verifyRequestWasCalledOnce(
+        arg: Networker.Target,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        guard verifyRequestArgs.count > 0 else {
+            return
+        }
+        
+        XCTAssertEqual(
+            verifyRequestArgs.count,
+            1,
+            file: file,
+            line: line
+        )
+        
+        XCTAssertEqual(
+            verifyRequestArgs.first,
+            arg,
+            file: file,
+            line: line
+        )
     }
 }
